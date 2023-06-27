1a68d5d481a018e4e835af2b47d09d0fac6f568c
visantos <vitoria.santos@confitec.com.br>
2023-06-27 16:02:53 -0300

    Order, Instant, ISO 8601 -part 1

--- src/main/java/com/devsuperior/cursoSpring/entities/User.java
+++ src/main/java/com/devsuperior/cursoSpring/entities/User.java
@@ -3,6 +3,8 @@ package com.devsuperior.cursoSpring.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
@@ -14,6 +16,8 @@ public class User {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders=new ArrayList<>();
 
     public User(){
     }
@@ -64,6 +68,10 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
