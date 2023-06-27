9b32220416f458f924c3fad9c78e07544930e4c8
visantos <vitoria.santos@confitec.com.br>
2023-06-26 23:17:29 -0300

    H2 database, test profile, JPA

--- src/main/java/com/devsuperior/cursoSpring/entities/User.java
+++ src/main/java/com/devsuperior/cursoSpring/entities/User.java
@@ -1,9 +1,14 @@
 package com.devsuperior.cursoSpring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
