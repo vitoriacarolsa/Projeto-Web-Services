4c230575ffe6c11e2290d0080883da318681d05e
visantos <vitoria.santos@confitec.com.br>
2023-06-27 13:57:09 -0300

    Service layer, component registration

--- src/main/java/com/devsuperior/cursoSpring/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/cursoSpring/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.devsuperior.cursoSpring.repositories;
 import com.devsuperior.cursoSpring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
