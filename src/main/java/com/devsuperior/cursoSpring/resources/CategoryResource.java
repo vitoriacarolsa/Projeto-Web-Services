1ad04bce8d0ffef6e0d979dd60a8cbc107cd736a
visantos <vitoria.santos@confitec.com.br>
2023-06-29 22:39:52 -0300

    User insert

--- src/main/java/com/devsuperior/cursoSpring/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/cursoSpring/resources/CategoryResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/Categorys")
+@RequestMapping(value = "/categorys")
 public class CategoryResource {
 
     @Autowired
--- src/main/java/com/devsuperior/cursoSpring/resources/ProductResource.java
+++ src/main/java/com/devsuperior/cursoSpring/resources/ProductResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/Products")
+@RequestMapping(value = "/products")
 public class ProductResource {
 
     @Autowired
